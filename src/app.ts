4dbb0a573989a1273c9f7690428291182e0f1076
DevByDipto <dipto.1900@gmail.com>
2025-10-29 22:01:18 +0600

    set orgin localhost

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ export const app = express()
 
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: 'http://localhost:5173',
      // ✅ নির্দিষ্ট করে দাও
     credentials: true,
   //   allowedHeaders: ['Content-Type', 'Authorization'],
